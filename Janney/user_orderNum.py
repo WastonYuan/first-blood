d53176a7e265400f7b663b5590095aef52d7c82d
wangxinyuan <wangxinyuan@zhihu.com>
2017-12-23 19:19:33 +0800

    fist task

---
+++ Janney/user_orderNum.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*
+#统计每个用户的订单数量
+from tools import local_file_util
+
+file =map(lambda line: line.split(','), local_file_util.readFile('bigdata/皇包车比赛/皇包车比赛数据-非压缩包/trainingset/orderHistory_train.csv')[1:])
+
+
+userId_orderId_list = map(lambda line:(line[0], line[1]), file)
+
+
+dic = {}
+
+#userid: list(orderid1, oderid2)
+
+for userId_orderId in userId_orderId_list:
+    if userId_orderId[0] in dic:
+        temp = dic[userId_orderId[0]]+ [userId_orderId[1]]
+        dic[userId_orderId[0]] = temp
+    else:
+        dic[userId_orderId[0]] = [userId_orderId[1]]
+
+
+userId_oderNum = sorted(map(lambda key: (key, dic[key].__len__()), dic), key=lambda tuple: tuple[1], reverse=True)
+
+res_save_str = map(lambda line: line[0] + '\t' + str(line[1]), userId_oderNum)
+
+local_file_util.writeFile('data/user_orderNum.tsv', res_save_str)
