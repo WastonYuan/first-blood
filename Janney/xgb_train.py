0c6c8da7a441bcaf4860bcfcb525f2fcc84f283a
wangxinyuan <wangxinyuan@zhihu.com>
2017-12-30 20:27:28 +0800

    new

--- Janney/xgb_train.py
+++ Janney/xgb_train.py
@@ -35,4 +35,13 @@ test_y = data[int(data.shape[0]*0.7):,0]
 dtrain = xgb.DMatrix(train_x, label=train_y)
 dtest = xgb.DMatrix(test_x, label=test_y)
 
-param = {'gamma':2.0,'nthread':8, 'max_depth':15, 'eta':0.03, 'silent':1, 'objective':'multi:softprob' ,'num_class':105}
+params = {'gamma':2.0,'nthread':8, 'max_depth':15, 'eta':0.03, 'silent':1, 'objective':'binary:logistic'}
+
+watchlist = [(dtest,'eval'), (dtrain,'train')]
+
+num_round = 100
+bst = xgb.train(params, dtrain, num_round, watchlist, early_stopping_rounds=50)
+
+bst.predict(dtrain)
+
+bst.save_model('xgb_model/xgb_v1.model')
