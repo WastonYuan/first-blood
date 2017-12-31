389f8884d72b609250cd4487a593394d9901113c
wangxinyuan <wangxinyuan@zhihu.com>
2017-12-31 11:29:25 +0800

    update

--- Janney/xgb_eval.py
+++ Janney/xgb_eval.py
@@ -1,7 +1,7 @@
 import xgboost as xgb
 
 bst = xgb.Booster({'nthread': 4})
-bst.load_model('data/xgb_model/xgb_v1.model')
+bst.load_model('xgb_model/xgb_v1.model')
 
 import numpy as np
 
@@ -13,5 +13,11 @@ dtest = xgb.DMatrix(test_x, label=test_y)
 
 eval = bst.predict(dtest)
 
+from tools import local_file_util
+file = [line.split(',')[0] for line in local_file_util.readFile('bigdata/huangbaoche/huangbaoche_unzip/test/orderFuture_test.csv')[1:]]
 
+res = [line[0] + ',' + str(line[1]) for line in zip(file, list(eval))]
+res.insert(0, 'userid,orderType')
+
+local_file_util.writeFile('data/submit.csv', res)
 
